2be93bfcf77f5eb2c62974260688d74944e5d206
Logan Cox <lmc@ou.edu>
2016-10-09 01:01:30 -0500

    Add comments.

--- onesearch/hosts.rb
+++ onesearch/hosts.rb
@@ -1,25 +1,18 @@
-  # Solr
-  config.vm.define( "solr") do |solr|
-    solr.vm.hostname = "solr.vagrant.local"
-    solr.vm.network "forwarded_port", guest:8443, host:8443
-    solr.vm.provision "shell",
-      path: "scripts/gethostinfo.sh", keep_color: "True",
-      run: "always"
-  end
+# See main Vagrantfile for default settings and provisioners
 
-  # Drupal 7
-  config.vm.define( "d7") do |d7|
-    d7.vm.hostname = "d7.vagrant.local"
-    d7.vm.provision "shell",
-      path: "scripts/gethostinfo.sh", keep_color: "True",
-      run: "always"
-  end
+# Solr
+config.vm.define( "solr") do |solr|
+  solr.vm.hostname = "solr.vagrant.local"
+  solr.vm.network "forwarded_port", guest:8443, host:8443
+end
 
-  # Nginx
-  config.vm.define( "nginx") do |nginx|
-    nginx.vm.hostname = "nginx.vagrant.local"
-    nginx.vm.network "forwarded_port", guest:443, host:64443
-    nginx.vm.provision "shell",
-      path: "scripts/gethostinfo.sh", keep_color: "True",
-      run: "always"
-  end
+# Drupal 7
+config.vm.define( "d7") do |d7|
+  d7.vm.hostname = "d7.vagrant.local"
+end
+
+# Nginx
+config.vm.define( "nginx") do |nginx|
+  nginx.vm.hostname = "nginx.vagrant.local"
+  nginx.vm.network "forwarded_port", guest:443, host:64443
+end
